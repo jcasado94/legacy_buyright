4126196e2e8604fb429809b5414b910286f2d617
Josep Casado <josepcasado94@gmail.com>
2018-12-21 13:57:13 +0900

    Refactor

--- go/src/gobuyright/pkg/server/response.go
+++ go/src/gobuyright/pkg/server/response.go
@@ -5,11 +5,13 @@ import (
 	"net/http"
 )
 
+// WriteError writes the error message msg as a json to w.
 func WriteError(w http.ResponseWriter, code int, msg string) {
-	WriteJson(w, code, map[string]string{"error": msg})
+	WriteJSON(w, code, map[string]string{"error": msg})
 }
 
-func WriteJson(w http.ResponseWriter, code int, payload interface{}) {
+// WriteJSON writes the given the payload as a json to w, setting the code as Header.
+func WriteJSON(w http.ResponseWriter, code int, payload interface{}) {
 	rsp, _ := json.Marshal(payload)
 
 	w.Header().Set("Content-Type", "application/json")
