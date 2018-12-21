f42ddc2e6c318ba3d85f892d7d48caf5442bd414
Josep Casado <josepcasado94@gmail.com>
2018-12-21 13:13:54 +0900

    Removed TagInput

--- generic-filler/src/components/Prod.js
+++ generic-filler/src/components/Prod.js
@@ -2,31 +2,10 @@ import React, { Component } from 'react';
 
 import ProdTitle from './ProdTitle'
 import ProdImage from './ProdImage'
-import TagInput from './TagInput';
 
-import { TAGS_TITLE, TAGS_PLACEHOLDER} from '../global/global'
 import ProdUsagesContainer from '../containers/ProdUsagesContainer';
 
 class Prod extends Component {
-
-  constructor() {
-    super();
-
-    this.state = {
-      prodTags: [],
-      prodTagsSuggestions: [
-        {id: 'one', text: 'One'},
-        {id: 'two', text: 'Two'},
-        {id: 'three', text: 'Three'},
-        {id: 'four', text: 'Four'},
-        {id: 'five', text: 'Five'},
-      ]
-    }
-
-    this.handleTagAddition = this.handleTagAddition.bind(this)
-    this.handleTagDelete = this.handleTagDelete.bind(this);
-
-  }
 
 	render() {
 		return (
@@ -34,40 +13,12 @@ class Prod extends Component {
         <ProdTitle />
         <ProdImage />
         <div className="prod-properties">
-          <TagInput
-            onTagAddition={this.handleTagAddition}
-            onTagDelete={this.handleTagDelete}
-            title={TAGS_TITLE}
-            placeholder={TAGS_PLACEHOLDER}
-            tagList={this.state.prodTags}
-            tagSuggestions={this.state.prodTagsSuggestions}
-          />
           <ProdUsagesContainer />
         </div>
       </div>
 		);
   }
 
-  handleTagAddition(tag) {
-    this.setState({
-      prodTags: listAddition(tag, this.state.prodTags)
-    })
-  }
-
-  handleTagDelete(i) {
-    this.setState({
-      prodTags: listDelete(i, this.state.prodTags)
-    })
-  }
-
-}
-
-const listAddition = (tag, list) => {
-  return [...list, tag]
-}
-
-const listDelete = (i, list) => {
-  return list.filter((tag, index) => index !== i)
 }
 
 
--- generic-filler/src/global/global.js
+++ generic-filler/src/global/global.js
@@ -3,9 +3,7 @@ import UsagesHandler from '../db/UsagesHandler';
 
 /* messages */
 
-export const TAGS_PLACEHOLDER = 'Add or create a new usage';
 export const USAGES_PLACEHOLDER = 'Add or create a new tag';
-export const TAGS_TITLE = 'Tags';
 export const USAGES_TITLE = 'Usages';
 
 
